61c71e32fb93489bfc3222809cfbc1b7e4850c37
HlDE1 <Natixbox@hotmail.de>
2023-02-21 01:22:09 +0100

    Starting Frontend

---
+++ react/src/components/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./NavigationBar.module.scss";
+
+const NavigationBar = () => {
+    return (
+        <div className={styles.test}>
+            <a>NavigationBar</a>
+        </div>
+    );
+};
+
+export default NavigationBar;
