3fb2f59f9201aefb19140e9412e1bb9e949f7bfe
HlDE1 <Natixbox@hotmail.de>
2023-02-23 02:50:58 +0100

    Setting up UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // $response = response()->json("Test");
+        return response()->json("User Index");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        User::create($request->validated());
+        return response()->json("User store");
+
+    }
+}
---
+++ database/migrations/2023_02_22_225946_create_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    //TODO: Create table for users here
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->name("firstname");
+            $table->name("surname");
+            $table->string("email");
+            $table->string("password");
+            $table->string("created_at");
+            $table->string("updated_at");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
